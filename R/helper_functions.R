bfbda9d7b82e150e1651825454f1b7e186230b3e
Prayag Gordy <prayaggordy@gmail.com>
2021-05-05 22:18:30 -0500

    add function to print latex

---
+++ R/helper_functions.R
@@ -0,0 +1,21 @@
+#' @title Print a fitted object from a step function as LaTeX
+print_latex_equation <- function(fit) {
+
+	x_vars <- fit$anova$Step %>%
+		gsub(pattern = "\\+ ", replacement = "", x = .) %>%
+		gsub(pattern = "\\_", replacement = " ", x = .) %>%
+		gsub(pattern = "\\:", replacement = "*", x = .) %>%
+		assertive::parenthesize()
+
+	y_var <- fit$call$formula[[2]] %>%
+		as.character() %>%
+		gsub(pattern = "\\_", replacement = " ", x = .) %>%
+		assertive::parenthesize()
+
+	sapply(2:length(x_vars),
+				 function(i) {
+				 	paste0("\\beta_{", i-1, "}", "\\text{", x_vars[i], "}")
+				 }) %>%
+		paste(collapse = " + ") %>%
+		paste0("$$", "\\text{", y_var, "}", " = ", "\\beta_0 + ", ., "$$")
+}
