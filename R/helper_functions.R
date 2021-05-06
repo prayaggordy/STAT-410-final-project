7ee2aace8b8f402006e6bedfa79926577c008a71
Prayag Gordy <prayaggordy@gmail.com>
2021-05-06 15:18:38 -0500

    equation as matrix

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -4,18 +4,11 @@ print_latex_equation <- function(fit) {
 	x_vars <- fit$anova$Step %>%
 		gsub(pattern = "\\+ ", replacement = "", x = .) %>%
 		gsub(pattern = "\\_", replacement = " ", x = .) %>%
-		gsub(pattern = "\\:", replacement = "*", x = .) %>%
-		assertive::parenthesize()
+		gsub(pattern = "\\:", replacement = "*", x = .)
 
 	y_var <- fit$call$formula[[2]] %>%
 		as.character() %>%
-		gsub(pattern = "\\_", replacement = " ", x = .) %>%
-		assertive::parenthesize()
+		gsub(pattern = "\\_", replacement = " ", x = .)
 
-	sapply(2:length(x_vars),
-				 function(i) {
-				 	paste0("\\beta_{", i-1, "}", "\\text{", x_vars[i], "}")
-				 }) %>%
-		paste(collapse = " + ") %>%
-		paste0("$$", "\\text{", y_var, "}", " = ", "\\beta_0 + ", ., "$$")
+	paste("$$", y_var, "=", "\\boldsymbol{\\beta}'", "\\begin{bmatrix}", paste0(x_vars[-1], collapse = "\\\\"), "\\end{bmatrix}", "+", "\\boldsymbol{\\epsilon}", "$$")
 }
