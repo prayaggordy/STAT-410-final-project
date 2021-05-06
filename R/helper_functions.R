d1091e795484453c7999f90815fe6697d44c3f6c
Prayag Gordy <prayaggordy@gmail.com>
2021-05-06 18:28:54 -0500

    fixed latex equation printer

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,14 +1,16 @@
 #' @title Print a fitted object from a step function as LaTeX
 print_latex_equation <- function(fit) {
 
-	x_vars <- fit$anova$Step %>%
-		gsub(pattern = "\\+ ", replacement = "", x = .) %>%
+	x_vars <- fit$call$formula[[3]] %>%
+		as.character() %>%
+		paste(collapse = " + ") %>%
 		gsub(pattern = "\\_", replacement = " ", x = .) %>%
-		gsub(pattern = "\\:", replacement = "*", x = .)
+		gsub(pattern = "\\:", replacement = "*", x = .) %>%
+		stringr::str_split(pattern = " \\+ ")
 
 	y_var <- fit$call$formula[[2]] %>%
 		as.character() %>%
 		gsub(pattern = "\\_", replacement = " ", x = .)
 
-	paste("$$", y_var, "=", "\\boldsymbol{\\beta}'", "\\begin{bmatrix}", paste0(x_vars[-1], collapse = "\\\\"), "\\end{bmatrix}", "+", "\\boldsymbol{\\epsilon}", "$$")
+	paste("$$", y_var, "=", "\\boldsymbol{\\beta}'", "\\begin{bmatrix}", paste0(x_vars[[1]][-1], collapse = "\\\\"), "\\end{bmatrix}", "+", "\\boldsymbol{\\epsilon}", "$$")
 }
